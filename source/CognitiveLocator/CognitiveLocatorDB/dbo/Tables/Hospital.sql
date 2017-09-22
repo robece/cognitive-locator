f4b8ef930fcc4efa1c85bc1e07f5249e4d287548
Daniel H. Courtois <dcourtois@live.com>
2017-09-21 21:15:47 -0500

    Se añaden los scripts de la base de datos y el modelo

---
+++ source/CognitiveLocator/CognitiveLocatorDB/dbo/Tables/Hospital.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Hospital] (
+    [IdHospital]  INT               IDENTITY (1, 1) NOT NULL,
+    [Hospital]    NVARCHAR (500)    NOT NULL,
+    [Estado]      NVARCHAR (50)     NOT NULL,
+    [Dirección]   NVARCHAR (500)    NULL,
+    [Coordenadas] [sys].[geography] NULL,
+    CONSTRAINT [PK_Hospital] PRIMARY KEY CLUSTERED ([IdHospital] ASC)
+);
+
