0bbba224a4c0da1a4aa58e1ca0fec4558e214671
OnlyD <islasvictorm@gmail.com>
2017-09-21 20:26:43 -0500

    Adding WEBAPI Project
    Agrego el proyecto de WEB API

---
+++ source/CognitiveLocator/CognitiveLocator.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CognitiveLocator.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
