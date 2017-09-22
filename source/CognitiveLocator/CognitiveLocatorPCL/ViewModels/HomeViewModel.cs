8c804d53842765accd2e3c338d7ef5c27d7dc5d2
Cristian Gonzalez <cristian@darrystonem.com>
2017-09-22 00:53:51 -0500

    Updated viewmodels for basic usage

--- source/CognitiveLocator/CognitiveLocatorPCL/ViewModels/HomeViewModel.cs
+++ source/CognitiveLocator/CognitiveLocatorPCL/ViewModels/HomeViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
+using System.Threading.Tasks;
+using CognitiveLocator.Views;
 using Xamarin.Forms;
 
 namespace CognitiveLocator.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        public Command NavigateToResultsCommand { get; set; }
+
         public HomeViewModel() : this(new DependencyServiceBase())
 		{
             
@@ -14,9 +17,20 @@ namespace CognitiveLocator.ViewModels
         public HomeViewModel(IDependencyService dependencyService) : base(dependencyService)
         {
 			DependencyService = dependencyService;
+            InitializeViewModel();
 		}
 
-        public override System.Threading.Tasks.Task OnViewAppear()
+        private void InitializeViewModel()
+        {
+            NavigateToResultsCommand = new Command(async () => await NavigateToResults());
+        }
+
+        private async Task NavigateToResults()
+        {
+            await NavigationService.PushModalAsync(new ResultsView());
+        }
+
+        public override Task OnViewAppear()
         {
             this.Title = "Hola a todos!";
             return base.OnViewAppear();
