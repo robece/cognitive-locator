10c856b31569663117813657f2d6dd23a2bfe1f9
analuciavazquez <dashurilu@hotmail.com>
2017-09-22 15:47:31 -0500

    Method generate  person

--- source/CognitiveLocator.WebAPI/Class/FaceAPIMethods.cs
+++ source/CognitiveLocator.WebAPI/Class/FaceAPIMethods.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Web;
 
 namespace CognitiveLocator.WebAPI.Class
@@ -16,9 +19,21 @@ namespace CognitiveLocator.WebAPI.Class
         /// </summary>
         /// <param name="personName"></param>
         /// <returns>Este metodo retorna un personId el cual debera ser almacenado en la base de datos</returns>
-        public bool AddNewMissingPeople(String personName)
+        public async System.Threading.Tasks.Task<bool> AddNewMissingPeopleAsync(String personName)
         {
             bool saved = false;
+            var client = new HttpClient();
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", FaceAPIKey);
+            var uri = "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+PersonGroupId+"/persons";
+            HttpResponseMessage response;
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{'name':'"+personName+"','userData':'Descripcion Ejemplo'}");
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
             return saved;
         }
 
@@ -28,9 +43,23 @@ namespace CognitiveLocator.WebAPI.Class
         /// <param name="stgUrl"></param>
         /// <param name="personId"></param>
         /// <returns>Este metodo retorna un FaceId para guardar en la base de datos</returns>
-        public bool AddFaceToMissingPeople(String stgUrl, String personId)
+        public async System.Threading.Tasks.Task<bool> AddFaceToMissingPeopleAsync(String stgUrl, String personId)
         {
             bool saved = false;
+            var client = new HttpClient();
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", FaceAPIKey);
+            // Request parameters
+            var uri = "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+PersonGroupId+"/persons/"+personId+"/persistedFaces?";
+            HttpResponseMessage response;
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{'url':'"+stgUrl+"'}");
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json >");
+                response = await client.PostAsync(uri, content);
+            }
+
             return saved;
         }
     }
