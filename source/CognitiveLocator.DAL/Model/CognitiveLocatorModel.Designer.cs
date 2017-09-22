eccc967dace1537af5ea6cd7d16120f144dbbd68
Baneforce <cristian_levi@outlook.com>
2017-09-22 15:01:22 -0500

    Se agrego la tabla PersonaFaces y la entidad en el modelo
    Se agregaron campos como Altura, Anchura, Izq y Der a tablas de Persona y PersonaFaces

--- source/CognitiveLocator.DAL/Model/CognitiveLocatorModel.Designer.cs
+++ source/CognitiveLocator.DAL/Model/CognitiveLocatorModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\dcourtois\Source\Repos\xamarin-cognitivelocator\source\CognitiveLocator\CognitiveLocatorDAL\Model\CognitiveLocatorModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\CognitiveLocator\xamarin-cognitivelocator\source\CognitiveLocator.DAL\Model\CognitiveLocatorModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ source/CognitiveLocator.DB/dbo/Tables/PersonaFaces.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PersonFaces]
+(
+	[IdPersonaFaces] INT NOT NULL , 
+    [idPersona] INT NOT NULL, 
+    [URL] NVARCHAR(500) NOT NULL, 
+    [FaceId] INT NULL, 
+	[Altura] FLOAT NULL, 
+    [Anchura] FLOAT NULL, 
+    [MargenIzquierdo] FLOAT NULL, 
+    [MargenDerecho] FLOAT NULL, 
+    CONSTRAINT [PK_PersonaFaces] PRIMARY KEY ([IdPersonaFaces] ASC), 
+    CONSTRAINT [FK_PersonFaces_Person] FOREIGN KEY ([idPersona]) REFERENCES [dbo].[Persona]([IdPersona]),
+
+)
