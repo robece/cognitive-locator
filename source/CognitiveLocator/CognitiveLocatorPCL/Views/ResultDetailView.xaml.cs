80e84b1c2ec1c10f1f201ac7c7d7932016e79852
Cristian Gonzalez <cristian@darrystonem.com>
2017-09-22 00:09:49 -0500

    Added Views

---
+++ source/CognitiveLocator/CognitiveLocatorPCL/Views/ResultDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CognitiveLocator.Views
+{
+    public partial class ResultDetailView : ContentPage
+    {
+        public ResultDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
