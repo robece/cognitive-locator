961c80deb9010ce7b47a423686495041b432394b
Gustavo de Jesús Barrientos Guerrero <gustavo.barrientos@acidstudios.me>
2017-09-22 01:43:32 -0500

    SearchPersonPage finished

--- source/CognitiveLocator/CognitiveLocatorPCL/ViewModels/SearchPersonViewModel.cs
+++ source/CognitiveLocator/CognitiveLocatorPCL/ViewModels/SearchPersonViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using CognitiveLocator.Models;
 
 namespace CognitiveLocator.ViewModels
 {
-    public class SearchPersonViewModel: BaseViewModel
+    public class SearchPersonViewModel : BaseViewModel
     {
         #region Properties
         Person _person;
@@ -12,10 +13,40 @@ namespace CognitiveLocator.ViewModels
             get { return _person; }
             set { SetProperty(ref _person, value); }
         }
+
+        ObservableCollection<Person> _results;
+        public ObservableCollection<Person> Results
+        {
+            get { return _results; }
+            set { SetProperty(ref _results, value); }
+        }
         #endregion
         public SearchPersonViewModel() : base(new DependencyServiceBase())
         {
             Title = "Buscar";
         }
+
+        #region Overrided Methods
+        public override System.Threading.Tasks.Task OnViewAppear()
+        {
+            // TODO: Load Results From WebAPI
+
+            var res = new ObservableCollection<Person>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                res.Add(new Person
+                {
+                    Nombre = "nombre",
+                    Apellidos = "apellidos",
+                    PhotoURL = "http://via.placeholder.com/150x150"
+                });
+            }
+
+            Results = res;
+
+            return base.OnViewAppear();
+        }
+        #endregion
     }
 }
--- source/CognitiveLocator/CognitiveLocatorPCL/Views/SearchPersonPage.xaml.cs
+++ source/CognitiveLocator/CognitiveLocatorPCL/Views/SearchPersonPage.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using CognitiveLocator.ViewModels;
 using Xamarin.Forms;
 
 namespace CognitiveLocator.Views
 {
     public partial class SearchPersonPage:BaseView
     {
+        SearchPersonViewModel _vm;
+        public SearchPersonViewModel ViewModel => _vm ?? (_vm = BindingContext as SearchPersonViewModel); 
+
         public SearchPersonPage()
         {
             InitializeComponent();
+            BindingContext = new SearchPersonViewModel();
         }
     }
 }
