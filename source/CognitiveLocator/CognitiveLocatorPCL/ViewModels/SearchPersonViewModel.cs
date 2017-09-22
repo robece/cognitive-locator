3f2f9f2ce436ad87a70ef0b03bdff4b2fd8b0891
Gustavo Barrientos <gustavo.barrientos@acidstudios.me>
2017-09-22 00:43:24 -0500

    SearchPersonPage and SearchPersonViewmodel added

---
+++ source/CognitiveLocator/CognitiveLocatorPCL/ViewModels/SearchPersonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CognitiveLocator.Models;
+
+namespace CognitiveLocator.ViewModels
+{
+    public class SearchPersonViewModel: BaseViewModel
+    {
+        #region Properties
+        Person _person;
+        public Person Person
+        {
+            get { return _person; }
+            set { SetProperty(ref _person, value); }
+        }
+        #endregion
+        public SearchPersonViewModel()
+        {
+            Title = "Buscar";
+        }
+    }
+}
