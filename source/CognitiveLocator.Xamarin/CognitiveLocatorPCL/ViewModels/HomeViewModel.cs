f38a7950fb1da369a5a0bc60e3b3d3a52ff642f3
Roberto Cervantes <robece@protonmail.com>
2017-09-23 14:43:58 -0500

    UI updates recovered

--- source/CognitiveLocator.Xamarin/CognitiveLocatorPCL/ViewModels/HomeViewModel.cs
+++ source/CognitiveLocator.Xamarin/CognitiveLocatorPCL/ViewModels/HomeViewModel.cs
@@ -9,28 +9,28 @@ namespace CognitiveLocator.ViewModels
     {
         public Command NavigateToResultsCommand { get; set; }
         public Command SearchPersonNameCommand { get; set; }
-		public Command SearchPersonPictureCommand { get; set; }
-		public Command CreateReportCommand { get; set; }
-		public Command AboutCommand { get; set; }
+        public Command SearchPersonPictureCommand { get; set; }
+        public Command CreateReportCommand { get; set; }
+        public Command AboutCommand { get; set; }
 
 
-		public HomeViewModel() : this(new DependencyServiceBase())
-		{
-            
-		}
+        public HomeViewModel() : this(new DependencyServiceBase())
+        {
+
+        }
 
         public HomeViewModel(IDependencyService dependencyService) : base(dependencyService)
         {
             Title = "Inicio";
-			DependencyService = dependencyService;
+            DependencyService = dependencyService;
             InitializeViewModel();
-		}
+        }
 
         private void InitializeViewModel()
         {
             NavigateToResultsCommand = new Command(async () => await NavigateToResults());
             SearchPersonNameCommand = new Command(async () => await SearchPersonByName());
-            SearchPersonPictureCommand= new Command(async () => await SearchPersonByPicture());
+            SearchPersonPictureCommand = new Command(async () => await SearchPersonByPicture());
             CreateReportCommand = new Command(async () => await CreateReport());
             AboutCommand = new Command(async () => await GoToAbout());
         }
@@ -50,10 +50,10 @@ namespace CognitiveLocator.ViewModels
             await NavigationService.PushAsync(new SearchPersonView("name"));
         }
 
-		private async Task SearchPersonByPicture()
-		{
+        private async Task SearchPersonByPicture()
+        {
             await NavigationService.PushAsync(new SearchPersonView("picture"));
-		}
+        }
 
         private async Task NavigateToResults()
         {
