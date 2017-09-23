aeaf80fc999d2555274e13469d0d71b75143f9b6
Baneforce <cristian_levi@outlook.com>
2017-09-22 20:00:19 -0500

    Se agrega el metodo para ejecutar cualquier SP

--- source/CognitiveLocator.DB/dbo/Tables/Hospital.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Hospital] (
-    [IdHospital]  INT               IDENTITY (1, 1) NOT NULL,
-    [HospitalName]    NVARCHAR (500)    NOT NULL,
-    [State]      NVARCHAR (50)     NOT NULL,
-    [Address]   NVARCHAR (500)    NULL,
-    [GeoLocation] [sys].[geography] NULL,
-    CONSTRAINT [PK_Hospital] PRIMARY KEY CLUSTERED ([IdHospital] ASC)
-);
-
