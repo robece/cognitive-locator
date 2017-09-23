c9ce527fc0bf4b6ce4ac36dc702715910e8f1d65
analuciavazquez <dashurilu@hotmail.com>
2017-09-22 22:44:03 -0500

    Change Controller API FACE

---
+++ source/CognitiveLocator.WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CognitiveLocator.WebAPI.Controllers
+{
+    [RoutePrefix("api/Find")]
+    public class SearchController : ApiController
+    {
+        [Route("ByFace")]
+        public async Task<HttpResponseMessage> ByFace()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        [Route("ByName")]
+        public async Task<HttpResponseMessage> ByName([FromUri] string name,[FromUri] string lastName)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+    }
+}
