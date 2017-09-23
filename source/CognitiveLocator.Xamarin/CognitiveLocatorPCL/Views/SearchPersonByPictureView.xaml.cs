56d9fa14568ac503a06cf314a36cee4138b91a67
Cristian Gonzalez <cristian@darrystonem.com>
2017-09-22 19:25:29 -0500

    Added Search By Picture

---
+++ source/CognitiveLocator.Xamarin/CognitiveLocatorPCL/Views/SearchPersonByPictureView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using CognitiveLocator.ViewModels;
+using Microsoft.Azure.Mobile.Analytics;
+using Xamarin.Forms;
+
+namespace CognitiveLocator.Views
+{
+    public partial class SearchPersonByPictureView : BaseView
+    {
+        public SearchPersonByPictureView()
+        {
+            InitializeComponent();
+            BindingContext = new SearchPersonByPictureViewModel();
+            Analytics.TrackEvent("View: Search By Picture");
+        }
+    }
+}
