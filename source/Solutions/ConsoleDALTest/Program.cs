14aad3b28358a1071a711bebf743bf88cffa4de8
Baneforce <cristian_levi@outlook.com>
2017-09-22 20:22:01 -0500

    Se agrego consola de pruebas y se subio la version del codigo a 4.6.2

---
+++ source/Solutions/ConsoleDALTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CognitiveLocatorDAL.Comunes;
+
+namespace ConsoleDALTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn = "Server=qbmjh35pyz.database.windows.net;Database=CognitiveLocator;User Id=rcervantes;Password = Password.1; ";
+            Sql aux = new Sql();
+            var res = aux.RunStoredProcParams(conn, "SelectAllPerson", null);
+        }
+    }
+}
