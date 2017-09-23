6e767487ad62c5757ea2972894584f05ff1cc923
Baneforce <cristian_levi@outlook.com>
2017-09-22 21:06:06 -0500

    Se genera metodo Async para ejecutar SPs y se cambia el campo a GUID para los IDs

--- source/CognitiveLocator.DAL/Comunes/sql.cs
+++ source/CognitiveLocator.DAL/Comunes/sql.cs
@@ -10,13 +10,11 @@ namespace CognitiveLocatorDAL.Comunes
 {
     public class Sql
     {
-        public IEnumerable<dynamic> RunStoredProcParams(string cnn, string sql, SqlParameter[] parameters = null)
+        public static IEnumerable<dynamic> RunStoredProcParams(string cnn, string sql, params SqlParameter[] parameters)
         {
             SqlConnection conn = null;
             SqlDataReader rdr = null;
             IEnumerable<dynamic> data = default(IEnumerable<dynamic>);
-
-            Console.WriteLine("\nCustomer Order History:\n");
 
             try
             {
@@ -38,6 +36,48 @@ namespace CognitiveLocatorDAL.Comunes
                 }
 
                 rdr = cmd.ExecuteReader();
+
+                data = rdr.Cast<IDataRecord>().ToList();
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+            }
+            return data;
+        }
+        public static async Task<IEnumerable<dynamic>> RunAsyncStoredProcParams(string cnn, string sql, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+            IEnumerable<dynamic> data = default(IEnumerable<dynamic>);
+
+            try
+            {
+                conn = new
+                    SqlConnection(cnn);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    sql, conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                {
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.Add(param);
+                    }
+                }
+
+                rdr =  await Task.Run(() => cmd.ExecuteReader());
 
                 data = rdr.Cast<IDataRecord>().ToList();
             }
