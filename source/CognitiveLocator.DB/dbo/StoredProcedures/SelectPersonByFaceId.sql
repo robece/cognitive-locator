79cb587fa10b56bb72549a4abdac06c5cfc48d78
Baneforce <cristian_levi@outlook.com>
2017-09-24 17:32:02 -0500

    Se elimino el campo Age de la tabla Person, entidad Person.cs y SPs

--- source/CognitiveLocator.DB/dbo/StoredProcedures/SelectPersonByFaceId.sql
+++ source/CognitiveLocator.DB/dbo/StoredProcedures/SelectPersonByFaceId.sql
@@ -3,7 +3,7 @@
 AS
 BEGIN
 	SET NOCOUNT ON;
-	SELECT IdPerson, IsFound, Name, LastName, Alias, Age,BirthDate,ReportedBy, Picture, Location, Notes, IsActive, FaceId FROM Person WHERE FaceId = @FaceId
+	SELECT IdPerson, IsFound, Name, LastName, Alias,BirthDate,ReportedBy, Picture, Location, Notes, IsActive, FaceId FROM Person WHERE FaceId = @FaceId
 END
 
 GO
