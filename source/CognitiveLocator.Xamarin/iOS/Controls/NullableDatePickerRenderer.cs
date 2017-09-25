6283005635e13374a59688f174d227dd5fe1c612
Humberto Jaimes <humberto.jaimes@outlook.com>
2017-09-24 19:41:19 -0500

    NullableDatePicker for birthday
    NullableDatePicker working in iOS
    *Android has bugs

---
+++ source/CognitiveLocator.Xamarin/iOS/Controls/NullableDatePickerRenderer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using CognitiveLocator.iOS.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CognitiveLocator.Views.Controls.NullableDatePicker), typeof(NullableDatePickerRenderer))]
+namespace CognitiveLocator.iOS.Controls
+{
+	public class NullableDatePickerRenderer : DatePickerRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null && this.Control != null)
+			{
+				this.AddClearButton();
+
+				
+
+				if (Device.Idiom == TargetIdiom.Tablet)
+				{
+					this.Control.Font = UIFont.SystemFontOfSize(25);
+				}
+			}
+
+		}
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+			if (this.Control == null) return;
+
+            var nullableDatePicker = this.Element as Views.Controls.NullableDatePicker;
+
+			if (nullableDatePicker != null)
+			{
+				switch (e.PropertyName)
+				{
+                    case nameof(nullableDatePicker.NullableDate):
+                        if (nullableDatePicker.NullableDate != null)
+                            this.Control.Text = nullableDatePicker.NullableDate.Value.ToString(nullableDatePicker.Format);
+                        else
+                        {
+                            this.Control.Placeholder = nullableDatePicker.Placeholder; 
+                            this.Control.Text = "";
+                        }
+                        break;
+				}
+			}
+        }
+
+		private void AddClearButton()
+		{
+			var originalToolbar = this.Control.InputAccessoryView as UIToolbar;
+
+			if (originalToolbar != null && originalToolbar.Items.Length <= 2)
+			{
+				var clearButton = new UIBarButtonItem("Ninguna", UIBarButtonItemStyle.Plain, ((sender, ev) =>
+				{
+                    Views.Controls.NullableDatePicker baseDatePicker = this.Element as Views.Controls.NullableDatePicker;
+					this.Element.Unfocus();
+					this.Element.Date = DateTime.Now;
+					baseDatePicker.CleanDate();
+
+				}));
+
+				var newItems = new List<UIBarButtonItem>();
+				foreach (var item in originalToolbar.Items)
+				{
+					newItems.Add(item);
+				}
+
+				newItems.Insert(0, clearButton);
+
+				originalToolbar.Items = newItems.ToArray();
+				originalToolbar.SetNeedsDisplay();
+			}
+
+		}
+	}
+}
