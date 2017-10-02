f9bc69b4b6ac8c5e5c82e8699cf87a12fe3ebb75
Roberto Cervantes <rcervantes@outlook.com>
2017-10-02 16:31:28 -0700

    Update cognitive-locator minor fixes

---
+++ source/CognitiveLocator.Xamarin/CognitiveLocator/Services/RestServices.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CognitiveLocator.Models.ApiModels;
+using CognitiveLocator.Services;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System.Collections.Generic;
+using CognitiveLocator.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+[assembly: Dependency(typeof(RestServices))]
+namespace CognitiveLocator.Services
+{
+    public class RestServices : IRestServices
+    {
+
+#if DEBUG
+        protected const string BaseURL = "http://cognitivelocatordev.azurewebsites.net/";
+#else
+        protected const string BaseURL = "http://cognitivelocator.azurewebsites.net/";
+#endif
+
+		public async Task<bool> CreateReportAsync(CreateReportModel model, byte[] photo)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var form = new MultipartFormDataContent())
+                    {
+                        var request = new HttpRequestMessage();
+                        request.Headers.TryAddWithoutValidation("content-type", "multipart/form-data");
+                        var imageContent = new ByteArrayContent(photo);
+                        imageContent.Headers.TryAddWithoutValidation("content-type", "image/jpeg"); //= new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpeg");
+                                                                                                    //content1.Add(imageContent,"photo",$"{Guid.NewGuid().ToString()}.jpg");
+                                                                                                    //request.Content = imageContent;
+                        request.Method = HttpMethod.Post;
+                        request.RequestUri = new Uri(BaseURL + "api/Photo/Post?"+ model.UrlFormat());
+
+                        HttpContent content = null;
+
+                        content = new ByteArrayContent(photo);
+                        content.Headers.TryAddWithoutValidation("content-type", "image/jpeg");
+                        content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                        {
+                            Name = "photo",
+                            FileName = $"{Guid.NewGuid().ToString()}.jpg"
+                        };
+                        form.Add(content);
+
+                        request.Content = form;
+                        using (var response = await client.SendAsync(request))
+                        {
+                            response.EnsureSuccessStatusCode();
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.InnerException?.ToString());
+                return false;
+            }
+        }
+
+        public async Task<List<Person>> SearchPersonByNameAsync(Person person)
+        {
+            return await SearchPersonByAsync($"ByName?name={person.Name}");
+        }
+
+        public async Task<List<Person>> SearchPersonByLastNameAsync(Person person)
+        {
+            return await SearchPersonByAsync($"ByLastName?lastName={person.LastName}");
+        }
+
+        public async Task<List<Person>> SearchByNameAndLastNameAsync(Person person)
+        {
+            return await SearchPersonByAsync($"ByNameAndLastName?name={person.Name}&lastName={person.LastName}");
+        }
+
+        public async Task<Person> SearchPersonByPhotoAsync(byte[] photo)
+        {
+            var result = new Person();
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					using (var form = new MultipartFormDataContent())
+					{
+                        var request = new HttpRequestMessage();
+						request.Headers.TryAddWithoutValidation("content-type", "multipart/form-data");
+						var imageContent = new ByteArrayContent(photo);
+						imageContent.Headers.TryAddWithoutValidation("content-type", "image/jpeg"); 
+						request.Method = HttpMethod.Post;
+						request.RequestUri = new Uri(BaseURL + "api/Find/ByFace");
+
+						HttpContent content = null;
+
+						content = new ByteArrayContent(photo);
+						content.Headers.TryAddWithoutValidation("content-type", "image/jpeg");
+						content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+						{
+							Name = "photo",
+							FileName = $"{Guid.NewGuid().ToString()}.jpg"
+						};
+						form.Add(content);
+
+						request.Content = form;
+                        using (var response = await client.SendAsync(request))
+						{
+							response.EnsureSuccessStatusCode();
+
+                            var json = await response.Content.ReadAsStringAsync();
+                            result = JsonConvert.DeserializeObject<Person>(json);
+						}
+					}
+				}
+
+                return result;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.InnerException?.ToString());
+                return null;
+			}
+        }
+
+        async Task<List<Person>> SearchPersonByAsync(string endpoint)
+        {
+            List<Person> results = new List<Person>();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var uri = $"{BaseURL}api/Find/{endpoint}";
+
+                    using(var response = await client.GetAsync(uri))
+                    {
+                        response.EnsureSuccessStatusCode();
+
+                        var json = await response.Content.ReadAsStringAsync();
+                        results = JsonConvert.DeserializeObject<List<Person>>(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine((ex?.InnerException?.ToString()));
+                return null;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ source/CognitiveLocator.Xamarin/CognitiveLocator/ViewModels/CreateReportViewModel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Threading.Tasks;
+using CognitiveLocator.Models.ApiModels;
+using CognitiveLocator.Services;
+using CognitiveLocator.Views;
+using Xamarin.Forms;
+
+namespace CognitiveLocator.ViewModels
+{
+    public class CreateReportViewModel : BaseViewModel
+    {
+        public Command SendReportCommand { get; set; }
+        public Command TakePhotoCommand { get; set; }
+        public Command ChoosePhotoCommand { get; set; }
+        public Command PreviewReportCommand { get; set; }
+
+        byte[] photo;
+        public byte[] Photo
+        {
+            get { return photo; }
+            set { SetProperty(ref photo, value); }
+        }
+
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { SetProperty(ref lastName, value); }
+        }
+
+        string alias;
+        public string Alias
+        {
+            get { return alias; }
+            set { SetProperty(ref alias, value); }
+        }
+
+        DateTime? birthday = null;
+        public DateTime? Birthday
+        {
+            get { return birthday; }
+            set { SetProperty(ref birthday, value); }
+        }
+
+        string location;
+        public string Location
+        {
+            get { return location; }
+            set { SetProperty(ref location, value); }
+        }
+
+        string notes;
+        public string Notes
+        {
+            get { return notes; }
+            set { SetProperty(ref notes, value); }
+        }
+
+        string reportedby;
+        public string ReportedBy
+        {
+            get { return reportedby; }
+            set { SetProperty(ref reportedby, value); }
+        }
+
+        public CreateReportViewModel() : this(new DependencyServiceBase())
+        {
+
+        }
+
+        public CreateReportViewModel(IDependencyService dependencyService) : base(dependencyService)
+        {
+            DependencyService = dependencyService;
+            InitializeViewModel();
+        }
+
+        private void InitializeViewModel()
+        {
+            Title = "Reportar persona";
+            PreviewReportCommand = new Command(async () => await PreviewReport());
+            SendReportCommand = new Command(async () => await SendReport());
+            TakePhotoCommand = new Command(async () => await TakePhoto());
+            ChoosePhotoCommand = new Command(async () => await ChoosePhoto());
+        }
+
+
+        private async Task ChoosePhoto()
+        {
+            Photo = await Helpers.MediaHelper.PickPhotoAsync();
+        }
+
+        private async Task TakePhoto()
+        {
+            Photo = await Helpers.MediaHelper.TakePhotoAsync();
+        }
+
+        private async Task SendReport()
+        {
+            var confirmation = await Application.Current.MainPage.DisplayAlert("Envío de información", "Confirmo que de manera bien intensionada estoy compartiendo una fotografía y datos personales de una persona extraviada.", "Si", "No");
+
+            if (!confirmation)
+                return;
+
+            var model = ValidateInformation();
+			if (!Plugin.Connectivity.CrossConnectivity.Current.IsConnected)
+				await Application.Current.MainPage.DisplayAlert("Error", "Es necesario tener conexión a internet para continuar.", "Aceptar");
+			else if (!IsBusy)
+            {
+                IsBusy = true;
+                if(await RestServices.CreateReportAsync(model, Photo))
+                    await NavigationService.PushAsync(new ReportConfirmationView());
+                else
+					await Application.Current.MainPage.DisplayAlert("Error", "No fue posible registrar el reporte, si el error persiste intenta mas tarde .", "Aceptar");
+
+				IsBusy = false;
+
+            }
+        }
+
+        private async Task PreviewReport()
+        {
+			var model = ValidateInformation();
+
+			if (model == null)
+				await Application.Current.MainPage.DisplayAlert("Error", "Por favor ingrese todo los datos obligatorios.", "Aceptar");
+			else if (!IsBusy)
+            {
+                IsBusy = true;
+                await NavigationService.PushAsync(new PreviewView(this));
+                IsBusy = false;
+            }
+        }
+
+        private CreateReportModel ValidateInformation()
+        {
+            var model = new CreateReportModel()
+            {
+                Name = this.Name,
+                LastName = this.LastName,
+                Alias = this.Alias,
+                BirthDate = this.Birthday,
+                Location = this.Location,
+                Notes = this.Notes,
+                ReportedBy = this.ReportedBy
+            };
+
+            if (Photo == null)
+                return null;
+            if (String.IsNullOrEmpty(model.Name))
+                return null;
+            if (String.IsNullOrEmpty(model.LastName))
+                return null;
+            if (String.IsNullOrEmpty(model.Location))
+				return null;
+            if (String.IsNullOrEmpty(model.ReportedBy))
+                return null;
+            
+            return model;
+        }
+    }
+}
