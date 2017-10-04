2fbc2b3e3e1db6ce480f9f0cc96fbdf063582bac
Roberto Cervantes <rcervantes@outlook.com>
2017-10-03 23:24:58 -0700

    Update https in settings

--- source/CognitiveLocator.Xamarin/CognitiveLocator/Settings.cs
+++ source/CognitiveLocator.Xamarin/CognitiveLocator/Settings.cs
@@ -6,11 +6,11 @@ namespace CognitiveLocator
         public static string MobileCenterID_Android = "f8963ef0-4239-46ea-b69c-621fafe09d0c";
         public static string MobileCenterID_iOS = "0da75977-ccf2-43fd-ba88-ae712f9a3568";
 
-        public static string GetBaseURL()
-        {
-            bool ProductionServices = false;
-            string BaseURL = (ProductionServices) ? "http://cognitivelocator.azurewebsites.net/" : "http://cognitivelocatordev.azurewebsites.net/";
-            return BaseURL;
-        }
+#if DEBUG
+        public const string BaseURL = "https://cognitivelocatordev.azurewebsites.net/";
+#else
+        public const string BaseURL = "https://cognitivelocator.azurewebsites.net/";
+#endif
+
     }
 }
