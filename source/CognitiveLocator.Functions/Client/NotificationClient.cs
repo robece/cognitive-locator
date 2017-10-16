90c0c1d65255f8af24bf439cf92b18087f37a532
Roberto Cervantes <robece@protonmail.com>
2017-10-16 16:59:25 -0500

    Code improvements after review

--- source/CognitiveLocator.Functions/Client/NotificationClient.cs
+++ source/CognitiveLocator.Functions/Client/NotificationClient.cs
@@ -8,13 +8,10 @@ namespace CognitiveLocator.Functions.Client
         public static NotificationClient Instance = new NotificationClient();
 
         public NotificationHubClient Hub { get; set; }
-
-        private string AccessSignature = Environment.GetEnvironmentVariable("NotificationHub_Access_Signature");
-        private string HubName = Environment.GetEnvironmentVariable("NotificationHub_Name");
-
+        
         private NotificationClient()
         {
-            Hub = NotificationHubClient.CreateClientFromConnectionString(AccessSignature, HubName);
+            Hub = NotificationHubClient.CreateClientFromConnectionString(Settings.NotificationAccessSignature, Settings.NotificationHubName);
         }
     }
 }
--- source/CognitiveLocator.Functions/Functions/StorageAccessKey.cs
+++ source/CognitiveLocator.Functions/Functions/StorageAccessKey.cs
@@ -14,25 +14,22 @@ namespace CognitiveLocator.Functions
 {
     public static class StorageAccessKey
     {
-        [FunctionName("StorageAccessKey")]
+        [FunctionName(nameof(StorageAccessKey))]
         public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "StorageAccessKey/")]HttpRequestMessage req, TraceWriter log)
         {
-            var azureWebJobsStorage = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            var cryptographyKey = Environment.GetEnvironmentVariable("Cryptography_Key");
-
             Domain.AccessStorageToken accessStorageToken = await req.Content.ReadAsAsync<Domain.AccessStorageToken>();
 
-            var decrypted_token = CryptoManager.Decrypt(accessStorageToken.Token, cryptographyKey);
+            var decrypted_token = CryptoManager.Decrypt(accessStorageToken.Token, Settings.CryptographyKey);
 
             byte[] data = Convert.FromBase64String(decrypted_token);
             DateTime when = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
-            if (when < DateTime.UtcNow.AddMinutes(-1))
+            if (when < DateTime.UtcNow.AddMinutes(-5))
             {
                 return req.CreateResponse(HttpStatusCode.BadRequest);
             }
 
-            return req.CreateResponse(HttpStatusCode.OK, azureWebJobsStorage);
+            return req.CreateResponse(HttpStatusCode.OK, Settings.AzureWebJobsStorage);
         }
     }
 }
