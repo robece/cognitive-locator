6bd3ef4ed5fdb1bdf843179f70c3e48c2c4e4deb
Roberto Cervantes <rcervantes@outlook.com>
2017-10-16 12:09:19 -0500

    Adding crypted token

---
+++ source/CognitiveLocator.Functions.Console/Managers/CryptoManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveLocator.Common
+{
+    public static class CryptoManager
+    {
+        private static byte[] CreateKey(string password, int keyBytes = 32)
+        {
+            byte[] salt = new byte[] { 80, 70, 60, 50, 40, 30, 20, 10 };
+            int iterations = 300;
+            var keyGenerator = new Rfc2898DeriveBytes(password, salt, iterations);
+            return keyGenerator.GetBytes(keyBytes);
+        }
+
+        public static string Encrypt(this string clearValue, string encryptionKey)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = CreateKey(encryptionKey);
+
+                byte[] encrypted = AesEncryptStringToBytes(clearValue, aes.Key, aes.IV);
+                return Convert.ToBase64String(encrypted) + ";" + Convert.ToBase64String(aes.IV);
+            }
+        }
+
+        private static byte[] AesEncryptStringToBytes(string plainText, byte[] key, byte[] iv)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException($"{nameof(plainText)}");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException($"{nameof(key)}");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException($"{nameof(iv)}");
+
+            byte[] encrypted;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(plainText);
+                    }
+                    encrypted = memoryStream.ToArray();
+                }
+            }
+            return encrypted;
+        }
+
+        public static string Decrypt(this string encryptedValue, string encryptionKey)
+        {
+            string iv = encryptedValue.Substring(encryptedValue.IndexOf(';') + 1, encryptedValue.Length - encryptedValue.IndexOf(';') - 1);
+            encryptedValue = encryptedValue.Substring(0, encryptedValue.IndexOf(';'));
+
+            return AesDecryptStringFromBytes(Convert.FromBase64String(encryptedValue), CreateKey(encryptionKey), Convert.FromBase64String(iv));
+        }
+
+        private static string AesDecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException($"{nameof(cipherText)}");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException($"{nameof(key)}");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException($"{nameof(iv)}");
+
+            string plaintext = null;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (MemoryStream memoryStream = new MemoryStream(cipherText))
+                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    plaintext = streamReader.ReadToEnd();
+
+            }
+            return plaintext;
+        }
+    }
+}
