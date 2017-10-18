6b0814e3c30fc5def53547d6d804effa897a4e63
Roberto Cervantes <robece@protonmail.com>
2017-10-18 17:09:46 -0500

    Security update and mobile settings function

---
+++ source/CognitiveLocator.Domain/Request/MetadataVerificationRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveLocator.Domain
+{
+    public class MetadataVerificationRequest : BaseRequest
+    {
+        public MetadataVerification Metadata { get; set; }
+    }
+}
